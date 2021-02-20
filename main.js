4db7af0d043d65aad1a87050cf766cb61d31573c
Supphakit Thepphong <Supphakit.tar@mail.kmutt.ac.th>
2021-02-21 02:00:22 +0700

    update

--- main.js
+++ main.js
@@ -52,27 +52,3 @@ const app = {
 }
 
 Vue.createApp(app).mount('#app');
-// methods: {
-//     showFavIcon(index) {
-//         this.pictures[index].showFav = !this.pictures[index].showFav;
-//     },
-//     showSearchBox() {
-//         this.showSearchBoxVar = !this.showSearchBoxVar;
-//         this.searchText='';
-//     },
-//     toggleModal(index){
-//         this.pictures[index].showModal = !this.pictures[index].showModal;
-//       }
-// },
-// computed: {
-//     countTotal() {
-//         return this.pictures.length;
-//     },
-//     countShow() {
-//         return this.pictures.filter(s => s.showFav).length;
-//     },
-//     search(){
-//         return this.pictures.filter(searchShow => {return searchShow.description.toLowerCase().includes(this.searchText.toLowerCase())});
-//     }
-// }
-// };
